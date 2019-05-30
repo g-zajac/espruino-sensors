39a92fc7a02c168d16ddcc3c206fad41d836771f
Grzegorz on Laptop <grzegorz.zajac@me.com>
2019-05-30 13:00:02 +0200

    add mqtt connection with test message

--- espruino-script.js
+++ espruino-script.js
@@ -2,8 +2,21 @@ var credentials = require("credentials");
 
 var WIFI_NAME = credentials.ssid;
 var WIFI_OPTIONS = { password: credentials.password };
+var MQTT_HOST = credentials.mqtthost;
+var MQTT_OPTIONS = {
+    // ALL OPTIONAL - the defaults are below
+    client_id: credentials.mqttclientid, // the client ID sent to MQTT - it's a good idea to define your own static one based on `getSerial()`
+    keep_alive: 60, // keep alive time in seconds
+    port: 1883, // port number
+    clean_session: true,
+    username: credentials.mqttuser,
+    password: credentials.mqttpassword,
+    protocol_name: "MQTT", // or MQIsdp, etc..
+    protocol_level: 4 // protocol level
+};
 
 var wifi;
+var mqtt = require("MQTT").create(MQTT_HOST, MQTT_OPTIONS);
 
 function onInit() {
     connectWiFi();
@@ -23,7 +36,28 @@ function connectWiFi() {
             console.log("WiFi connected");
             wifi.getIP(function(f, ip) {
                 console.log("IP: ", ip);
+                mqttConnect();
             });
         }
     );
 }
+
+function mqttConnect() {
+    mqtt.connect();
+    mqtt.on("connected", function() {
+        console.log("MQTT connected!");
+    });
+    mqtt.on("publish", "test");
+    mqtt.on("disconnected", function() {
+        console.log("MQTT disconnected...reconecting");
+        setTimeout(function() {
+            mqtt.connect();
+        }, 1000);
+    });
+}
+
+setInterval(function() {
+    if (!mqtt) return;
+    console.log("publishing mqtt cpu temp: ", E.getTemperature());
+    mqtt.publish("cputemp", E.getTemperature());
+}, 10 * 1000);
